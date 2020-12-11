26eea94b6ec942394ac2e838824daf5bf4503200
selfsryo <selfsryo@gmail.com>
2020-12-12 02:10:06 +0900

    タブをスペースに更新

--- blog/admin.py
+++ blog/admin.py
@@ -4,16 +4,16 @@ from .models import Tag
 from .models import Post
 
 class TagAdmin(admin.ModelAdmin):
-	list_display = ('id', 'name', 'color')
+    list_display = ('id', 'name', 'color')
 
 
 class PostAdmin(MarkdownxModelAdmin):
-	list_display = ('title', 'get_tag', 'slug', 'is_public', 'created_at', 'updated_at')
-	list_display_links = list_display
-	ordering = ('-created_at',)
-	
-	def get_tag(self, row):
-		return ','.join([x.name for x in row.tag.all()])
+    list_display = ('title', 'get_tag', 'slug', 'is_public', 'created_at', 'updated_at')
+    list_display_links = list_display
+    ordering = ('-created_at',)
+    
+    def get_tag(self, row):
+        return ','.join([x.name for x in row.tag.all()])
 
 admin.site.register(Tag, TagAdmin)
 admin.site.register(Post, PostAdmin)
