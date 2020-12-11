6df8c89f73f21ec8eecffefd2e719aa64e77c8b6
selfsryo <selfsryo@gmail.com>
2020-12-12 01:58:15 +0900

    タブをスペースに変更

--- blog/views.py
+++ blog/views.py
@@ -8,38 +8,38 @@ from .serializers import TagSerializer, PostSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
-	queryset = Tag.objects.all()
-	serializer_class = TagSerializer
-	permission_classes = (IsAdminOrReadOnly,)
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (IsAdminOrReadOnly,)
 
 
 class CustomPagination(pagination.PageNumberPagination):
-	page_size = 10
+    page_size = 10
 
-	def get_paginated_response(self, data):
-		"""ページネーションをカスタマイズ"""
-		return response.Response({
-			'next': self.get_next_link(),
-			'previous': self.get_previous_link(),
-			'total_pages': self.page.paginator.num_pages,
-			'current_page': self.page.number,
-			'results': data,
-		})
+    def get_paginated_response(self, data):
+        """ページネーションをカスタマイズ"""
+        return response.Response({
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'total_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data,
+        })
 
 
 class PostViewSet(viewsets.ModelViewSet):
-	queryset = Post.objects.filter(is_public=True)
-	serializer_class = PostSerializer
-	permission_classes = (IsAdminOrReadOnly,)
-	pagination_class = CustomPagination
-	lookup_field = 'slug'
+    queryset = Post.objects.filter(is_public=True)
+    serializer_class = PostSerializer
+    permission_classes = (IsAdminOrReadOnly,)
+    pagination_class = CustomPagination
+    lookup_field = 'slug'
 
-	def get_queryset(self):
-		"""GETのパラメータに対応したクエリセットを返す"""
-		queryset = super().get_queryset()
+    def get_queryset(self):
+        """GETのパラメータに対応したクエリセットを返す"""
+        queryset = super().get_queryset()
 
-		tag = self.request.query_params.get('tag', None)
-		if tag:
-			queryset = queryset.filter(tag=tag)
+        tag = self.request.query_params.get('tag', None)
+        if tag:
+            queryset = queryset.filter(tag=tag)
 
-		return queryset
+        return queryset
